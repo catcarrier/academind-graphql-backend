4c4b0cc02a6687efa4740d9e16eb92e999f71375
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-11-29 12:17:04 -0600

    refactored Promises to async/await

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,112 +4,103 @@ const jwt = require('jsonwebtoken');
 
 const { validationResult } = require('express-validator/check');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
 
-    // check validation errors returned by the route handler
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const error = new Error(errors.array()[0]);
-        error.statusCode = 422;
-        error.data = errors.array();
-        throw error;
+    try {
+        // check validation errors returned by the route handler
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const err = new Error(errors.array()[0]);
+            err.statusCode = 422;
+            err.data = errors.array();
+            throw err;
+        }
+
+        const email = req.body.email;
+        const name = req.body.name;
+        const password = req.body.password;
+
+        const hash = await bcrypt.hash(password, 12);
+        let user = new User({
+            email: email,
+            password: hash,
+            name: name
+        });
+        user = await user.save();
+        res.status(201).json({ message: 'User created', userId: user._id });
+    } catch (e) {
+        if (!e.statusCode) {
+            e.statusCode = 500;
+        }
+        next(e);
     }
-
-    email = req.body.email;
-    name = req.body.name;
-    password = req.body.password;
-
-    // encrypt the password
-    bcrypt
-        .hash(password, 12)
-        .then(hash => {
-            const user = new User({
-                email: email,
-                password: hash,
-                name: name
-            });
-            return user.save();
-        })
-        .then(user => {
-            res.status(201).json({ message: 'User created', userId: user._id });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        })
-
 }
 
-exports.login = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    let loadedUser;
+exports.login = async (req, res, next) => {
 
-    User.findOne({ email: email })
-        .then(user => {
-            if (!user) {
-                const err = new Error('No user matching this email');
-                err.statusCode = 401;
-                next(err);
-            }
-            loadedUser = user;
-            return bcrypt.compare(password, user.password)
-        })
-        .then(match => {
-            if (!match) {
-                const err = new Error('Password does not match');
-                err.statusCode = 401;
-                next(err);
-            }
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const user = await User.findOne({ email: email });
 
-            // TODO import the secret and reuse
-            const token = jwt.sign({
-                email: loadedUser.email,
-                userId: loadedUser._id.toString()
-            }, 'secret secret hush hush on the QT', { expiresIn: '1h' });
-            res.status(200).json({ token: token, userId: loadedUser._id.toString() })
-        })
-        .catch(err => {
-            if (!err.statusCode) { err.statuscode = 500 }
-            next(err);
-        })
+        if (!user) {
+            const err = new Error('No user matching email ' + email);
+            err.statusCode = 401;
+            throw err;
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            const err = new Error('Password does not match');
+            err.statusCode = 401;
+            throw err;
+        }
+
+        // TODO import the secret from a shared resource, file etc
+        const token = jwt.sign({
+            email: user.email,
+            userId: user._id.toString()
+        }, 'secret secret hush hush on the QT', { expiresIn: '1h' });
+
+        res.status(200).json({ token: token, userId: user._id.toString() })
+    } catch (e) {
+        if (!e.statusCode) { e.statusCode = 500 }
+        next(e);
+    }
 
 };
 
-exports.getUserStatus = (req, res, next) => {
-    return User.findOne({_id:req.userId})
-        .then(user => {
-            if(!user) {
-                const error = new Error('No such user.');
-                error.statusCode = 401;
-                throw err;
-            }
-            return user;
-        })
-        .then(user => {
-            res.status(200).json({status: user.status});
-        })
-        .catch(err => {
-            const error = new Error('Unable to locate a matching user.');
-            error.statusCode = 401;
-            next(err);
-        })
+exports.getUserStatus = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ _id: req.userId });
+        if (!user) {
+            const err = new Error('No such user.');
+            err.statusCode = 404;
+            throw err;
+        }
+        res.status(200).json({ status: user.status });
+    } catch (e) {
+        if (!e.statusCode) { e.statusCode = 401; }
+        next(err);
+    }
 }
 
-exports.setUserStatus = (req, res, next) => {
+exports.setUserStatus = async (req, res, next) => {
     let status = req.body.status;
-    return User.findOne({_id:req.userId})
-        .then(user => {
-            user.status = status;
-            return user.save();
-        })
-        .then(user => {
-            res.status(200).json({message: "OK", status: status});
-        })
-        .catch(err => {
-            if(!error.statusCode) {error.statusCode = 500;}
+
+    try {
+        let user = await User.findOne({ _id: req.userId });
+        if (!user) {
+            const err = new Error('No such user.');
+            err.statusCode = 404;
             throw err;
-        })
+        }
+        user.status = status;
+        await user.save();
+        res.status(200).json({ message: "OK", status: status });
+    } catch (e) {
+        if (!e.statusCode) { e.statusCode = 500; }
+        throw e;
+    }
 }
--- controllers/feed.js
+++ controllers/feed.js
@@ -4,127 +4,96 @@ const { validationResult } = require('express-validator/check');
 const User = require('../models/user');
 const Post = require('../models/post');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1;
     const PER_PAGE = 2; // TODO share between back and front end
-    let totalItems;
-    Post.find()
-        .countDocuments()
-        .then(result => {
-            totalItems = result;
-            return Post.find()
-                .skip((currentPage - 1) * PER_PAGE)
-                .limit(PER_PAGE);
-        })
-        .then(posts => {
-            res.status(200).json({
-                message: 'OK',
-                posts: posts,
-                totalItems: totalItems
-            });
-        })
-        .catch(err => {
-            if (!err.statusCode) { err.statusCode = 500; }
-            return next(err);
+
+    try {
+        let totalItems = await Post.find().countDocuments();
+        const posts = await Post.find()
+            .populate('creator')
+            .skip((currentPage - 1) * PER_PAGE)
+            .limit(PER_PAGE);
+        res.status(200).json({
+            message: 'OK',
+            posts: posts,
+            totalItems: totalItems
         });
+    } catch (e) {
+        if (!e.statusCode) { e.statusCode = 500; }
+        next(e);
+    }
 }
 
-exports.getPost = (req, res, next) => {
-
-    //console.log('req.params is -> ', req.params)
-
-    //console.log('req.params.postId -> ', req.params.postId)
-
-    const postId = req.params.postId;
-    Post.findById({ _id: require('mongoose').Types.ObjectId(postId) })
-        .then(post => {
-            //console.log(post);
-            if (!post) {
-                const error = new Error('No such post!')
-                error.statusCode = 404;
-                throw error;
-            }
-            res.status(200).json({ message: "ok", post: post });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        })
+exports.getPost = async (req, res, next) => {
+    try {
+        const postId = req.params.postId;
+        const post = await Post.findById({ _id: require('mongoose').Types.ObjectId(postId) });
+        if (!post) {
+            const err = new Error('No post found matching id ' + postId);
+            err.statusCode = 404;
+            throw err;
+        }
+        res.status(200).json({ message: "ok", post: post });
+    } catch (e) {
+        if (!e.statusCode) {
+            e.statusCode = 500;
+        }
+        next(e);
+    }
 }
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         const error = new Error('Validation failed - data not correct');
         error.statusCode = 422;
-        throw (error); // synchronous, use throw
+        throw error;
     }
 
     // file upload is required
     if (!req.file) {
         const error = new Error('Image upload is required, not found');
         error.statusCode = 422;
-        throw (error);
+        throw error;
     }
 
     const title = req.body.title;
     const content = req.body.content;
     const imageUrl = req.file.path.replace(/\\/g, '/');
 
-    const post = new Post({
+    let post = new Post({
         title: title,
         imageUrl: imageUrl,
         content: content,
         creator: req.userId // mongoose will convert to ObjectId
     })
 
-    let creator;
-
-    post.save()
-        .then(result => {
-            return User.findOne({ _id: req.userId });
+    try {
+        post = await post.save();
+        // add this post to the user's history
+        const user = await User.findOne({ _id: req.userId });
+        user.posts.push(post._id);
+        await user.save();
+        res.status(201).json({
+            message: "Post created OK",
+            post: post,
+            creator: { _id: user._id, name: user.name }
         })
-        .then(user => {
-            if (!user) {
-                const error = new Error('User not found');
-                error.statusCode = 401;
-                return next(error);
-            }
-            creator = user;
-            return user;
-        })
-        .then(user => {
-            // Add this post to the user's list
-            user.posts.push(post)
-            return user.save();
-        })
-        .then(result => {
-            res.status(201).json({
-                message: "Post created OK",
-                post: post,
-                creator: { _id: creator._id, name: creator.name }
-            })
-        })
-        .catch(err => {
-            // If there is no status code, assign a 500-range.
-            // Cannot throw here, as we are in a promise chain, have to call next()
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        })
+    } catch (e) {
+        if (!e.statusCode) {
+            e.statusCode = 500;
+        }
+        next(e);
+    }
 }
 
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const error = new Error('Validation failed - data not correct');
-        error.statusCode = 422;
-
-        // This is synchronous code, OK to throw
-        throw (error);
+        const err = new Error('Validation failed - data not correct');
+        err.statusCode = 422;
+        throw err;
     }
 
     const postId = req.params.postId;
@@ -142,114 +111,93 @@ exports.updatePost = (req, res, next) => {
 
     // At least one of the above should be true, so imageUrl should be set.
     if (!imageUrl) {
-        const error = new Error('No image file given for this post');
-        error.errorCode = 422;
-        throw (err);
+        const err = new Error('No image file given for this post');
+        err.errorCode = 422;
+        throw err;
     }
 
-    Post.findById({ _id: require('mongoose').Types.ObjectId(postId) })
-        .then(post => {
-            if (!post) {
-                const error = new Error('No matching post (' + postId + ')');
-                error.errorCode = 404;
-                throw error;
-            }
-            return post;
-        })
-        .then(post => {
-            // Validate that the current user is the creator of this post.
-            if(post.creator.toString() !== req.userId) {
-                const error = new Error('Not allowed to edit this post.');
-                error.errorCode = 403;
-                throw error;
-            }
-            return post;
-        })
-        .then(post => {
-            // If the user sent a new file, these two will not match, 
-            // and we whack the old one.
-            if (imageUrl /* new value */ != post.imageUrl /* old value */) {
-                clearImage(post.imageUrl)
-            }
+    try {
+        let post = await Post.findById({ _id: require('mongoose').Types.ObjectId(postId) })
+        if (!post) {
+            const err = new Error('No post matching id ' + postId);
+            err.errorCode = 404;
+            throw err;
+        }
 
-            post.title = title;
-            post.content = content;
-            post.imageUrl = imageUrl;
+        // Validate that the current user is the creator of this post.
+        if (post.creator.toString() !== req.userId) {
+            const err = new Error('Not allowed to edit this post.');
+            err.errorCode = 403;
+            throw err;
+        }
 
-            //console.log('new image ', imageUrl)
+        // If the user sent a new file, these two will not match, 
+        // and we whack the old one.
+        if (imageUrl /* new value */ != post.imageUrl /* old value */) {
+            clearImage(post.imageUrl)
+        }
 
-            return post.save();
-        })
+        post.title = title;
+        post.content = content;
+        post.imageUrl = imageUrl;
 
-        .then(post => {
-            res.status(200).json({ message: 'Post updated', post: post });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err); // use next() in asynch, throw() in synch
-        })
-
-
+        post = await post.save();
+        res.status(200).json({ message: 'Post updated', post: post });
+    } catch (e) {
+        if (!e.statusCode) {
+            e.statusCode = 500;
+        }
+        next(e);
+    }
 }
 
-exports.deletePost = (req, res, next) => {
+exports.deletePost = async (req, res, next) => {
     const postId = req.params.postId;
-    Post.findById(postId)
-        .then(post => {
-            if (!post) {
-                const error = new Error('No matching post (' + postId + ')');
-                error.errorCode = 404;
-                throw error;
-            }
-            return post;
-        })
-        .then(post => {
-            // Only the post's creator can delete
-            if(post.creator.toString() !== req.userId) {
-                const error = new Error('Not allowed to delete this post.');
-                error.errorCode = 403;
-                throw error;
-            }
-            return post;
-        })
-        .then( post =>{
-            // whack the image
-            clearImage(post.imageUrl);
-            return Post.findByIdAndRemove(postId);
-        })
-        .then(result => {
-            // Find the user who posted that, and remove it from their post history.
-            // Note that we do not assume it is the current user's User object. This
-            // a precaution in case the post deletion (above) becomes role-based, and
-            // not strictly limited to the post's author.
-            // Note the assumption that the post appears in exactly one user's post history.
-            let postObjectId = require('mongoose').Types.ObjectId(postId);
-            return User.findOne({posts: postObjectId})
-                .then(user => {
-                    if(!user){
-                        const error = new Error('Unable to remove this post from the user history.');
-                        error.errorCode = 500;
-                        throw error;
-                    }
-                    return user;
-                })
-                .then(user => {
-                    var index = user.posts.indexOf(postObjectId);
-                    user.posts.splice(index, 1);
-                    return user.save();
-                })
-        })
-        .then(result => {  
-            res.status(200).json({ message: 'Post deleted' });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        })
+
+    try {
+        const post = await Post.findById(postId);
+        if (!post) {
+            const err = new Error('No matching post (' + postId + ')');
+            err.errorCode = 404;
+            throw err;
+        }
+
+        // Only the post's creator can delete
+        if (post.creator.toString() !== req.userId) {
+            const err = new Error('Not allowed to delete this post.');
+            err.errorCode = 403;
+            throw err;
+        }
+
+        clearImage(post.imageUrl);
+
+        await Post.findByIdAndRemove(postId);
+
+        // Find the user who posted that, and remove it from their post history.
+        // Note that we do not assume it is the current user's User object. This
+        // a precaution in case the post deletion (above) becomes role-based, and
+        // not strictly limited to the post's author.
+        // Note the assumption that the post appears in exactly one user's post history.
+        let postObjectId = require('mongoose').Types.ObjectId(postId);
+        const user = await User.findOne({ posts: postObjectId });
+
+        if (!user) {
+            const err = new Error('Unable to remove this post from the user history - user not found.');
+            err.errorCode = 500;
+            throw err;
+        }
+
+        var index = user.posts.indexOf(postObjectId);
+        user.posts.splice(index, 1);
+        await user.save();
+
+        res.status(200).json({ message: 'Post deleted' });
+    } catch (e) {
+        if (!e.statusCode) {
+            e.statusCode = 500;
+        }
+        next(e);
+    }
 }
 
 const clearImage = (filePath) => {
